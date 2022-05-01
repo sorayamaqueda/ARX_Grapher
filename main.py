005b1fc4f72c8e2f69249af6ba542ab3a0c6a298
Soraya Lizeth Maqueda Gutiérrez <A01411195@itesm.mx>
2022-05-01 18:15:19 -0500

    Defined coeffiecients and Input Texts

--- main.py
+++ main.py
@@ -1,24 +1,71 @@
 import PySimpleGUI as sg # Graphic Interface Library
 import numpy as np # Math Library
 import pandas as pd # Data Analysis Library
+import matplotlib # Grapher Library
+
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-import matplotlib
-import oceans
+from math import e, trunc
 
 matplotlib.use('TkAgg')
+
+# Global Contro Variables
+defaultSize = (15, 1)
+
+numA = 0 # Number of a's coefficients
+numB = 0 # Number of b's coefficients
+numY = 0 # Number of outputs
+numU = 0 # Number of inputs
 
 # Function to draw Graph 
 def draw_figure(canvas, figure):
     figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
     figure_canvas_agg.draw()
     figure_canvas_agg.get_tk_widget().pack(side='top', fill='both', expand=1)
+
     return figure_canvas_agg
+
+################### ARX Mode ###################
+
+# Case Determiner
+def case(d, T):
+    tPrime = d*T
+    
+    if tPrime % T == 0:
+        # Case 1: tPrime is multiple of T
+        return True
+    else:
+        # Case 2: there's a delay theta
+        return False
+
+def a(T, tao):
+    a1 = e**(-T /tao)
+    print(a1)
+
+def b(T, tao, k):
+    b1 = k*(1-(e**(-T/tao)))
+
+# Delay
+def d(tPrime, T):
+    return trunc(tPrime/T)
+
+# Theta
+def t(tPrime, T, d):
+    return tPrime-(d*T)
 
 # Define the Layout
 layout = [
+    # Canvas
     [sg.Text('ARX Graphic')],
     [sg.Canvas(key='-CANVAS-')],
-    [sg.Button('OK')]
+    [sg.Button('Close')],
+    # Input Values
+    [sg.Text('Enter Values')],
+    [sg.Text('Constant (k)', size=defaultSize), sg.InputText(key='-k-')],
+    [sg.Text('Delay (d)', size=defaultSize), sg.InputText(key='-d-')],
+    [sg.Text("\u03F4'", size=defaultSize), sg.InputText(key='-tPrime-')],
+    [sg.Text('Time Interval (T)', size=defaultSize), sg.InputText(key='-T-')],
+    [sg.Text('Time Constant (\u03F4)', size=defaultSize), sg.InputText(key='-t-')],
+    [sg.Button('Submit')]
 ]
 
 # Create the window
@@ -43,8 +90,18 @@ draw_figure(window['-CANVAS-'].TKCanvas, fig)
 while True:
     event, values = window.read()
     # End program if user closes window or
-    # presses the OK button
-    if event == 'OK' or event == sg.WIN_CLOSED:
+    # presses the Close button
+    if event == 'Close' or event == sg.WIN_CLOSED:
         break
 
+    if event == 'Submit':
+        values=['-k-', '-T-', '-tPrime-', '-d-', '-t-']
+        print(values)
+        a(values['-T-'], values['-t-'])
+
 window.close()
+
+# References
+# https://apmonitor.com/wiki/index.php/Apps/ARXTimeSeries
+# https://apmonitor.com/do/index.php/Main/ModelIdentification
+# https://www.youtube.com/watch?v=8HdUoWXLNOs
