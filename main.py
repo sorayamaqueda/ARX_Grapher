9f860392f7cfa84604fd9ebd4d67b6a73768c314
Soraya Lizeth Maqueda Gutiérrez <A01411195@itesm.mx>
2022-05-18 22:37:09 -0500

    Reorganized frames

--- main.py
+++ main.py
@@ -12,11 +12,14 @@
 # Based on an Excel document, we need to preview how the graph will be.
 # The first delivery needs to receive inputs from User, and then be able to graph a first order ARX Model.
 
+import control.matlab as control
+import scipy.signal as signal
 import matplotlib.pyplot as plt
-import PySimpleGUI as sg # Graphic Interface Library
-import pandas as pd # Data Analysis Library
-import numpy as np # Math Library
-import matplotlib # Grapher Library
+import PySimpleGUI as sg  # Graphic Interface Library
+import seaborn as sns  # Library for decorations
+import pandas as pd  # Data Analysis Library
+import numpy as np  # Math Library
+import matplotlib  # Grapher Library
 
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from math import e, trunc
@@ -27,20 +30,20 @@ matplotlib.use('TkAgg')
 defaultSize = (10, 1)
 
 maxCoefficientIndex = 4
-numA = 0 # Number of a's coefficients
-numB = 0 # Number of b's coefficients
-numY = 0 # Number of outputs
-numU = 0 # Number of inputs
+numA = 0  # Number of a's coefficients
+numB = 0  # Number of b's coefficients
+numY = 0  # Number of outputs
+numU = 0  # Number of inputs
 
 # Input Values
-k = 0 # Gain Constant
-T = 0 # Time Interval
-tau = 0 # Time Constant
-t = 0 # Theta
-tPrime = 0 # Theta Prime
-inDist = 0 # Input Disturbance
-outDist = 0 # Output Disturbance
-mk = 0 # Input Signal
+k = 0  # Gain Constant
+T = 0  # Time Interval
+tau = 0  # Time Constant
+t = 0  # Theta
+tPrime = 0  # Theta Prime
+inDist = 0  # Input Disturbance
+outDist = 0  # Output Disturbance
+mk = 0  # Input Signal
 
 # Coefficients (Maximum 4 per coefficient)
 # If not given, value must be 0
@@ -54,7 +57,9 @@ b2 = 0
 b3 = 0
 b4 = 0
 
-# Function to draw Graph 
+# Function to draw Graph
+
+
 def draw_figure(canvas, figure):
     figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
     figure_canvas_agg.draw()
@@ -65,9 +70,11 @@ def draw_figure(canvas, figure):
 ################### Plant Control Discrete Model ###################
 
 # Case Determiner
+
+
 def case(d, T):
     tPrime = d*T
-    
+
     if tPrime % T == 0:
         # Case 1: tPrime is multiple of T
         return True
@@ -75,27 +82,38 @@ def case(d, T):
         # Case 2: there's a delay theta
         return False
 
+
 def an(T, tao):
-    return e**(-T /tao)
+    return e**(-T / tao)
+
 
 def bn(T, tao, k):
     return k*(1-(e**(-T/tao)))
 
 # Delay
+
+
 def d(tPrime, T):
     return trunc(tPrime/T)
 
 # Theta
+
+
 def t(tPrime, T, d):
     return tPrime-(d*T)
 
 # Theta Prime
+
+
 def tPrime(t, T, d):
     return t+(d*T)
 
 # Transfer Function
+
+
 def HGp(cn, mn):
     return cn/mn
+
 
 def mn(k, t, T, z, d):
     return k*(1-(e**(-T/t)))*(z**(-1-d))
@@ -103,6 +121,8 @@ def mn(k, t, T, z, d):
 # Difference Equations
 
 # c[n] = a1*c[n-1] + b1*m[n-1-d]
+
+
 def cn(kMax, T, d, tao):
     cn = []
     a1 = an(T, tao)
@@ -110,13 +130,15 @@ def cn(kMax, T, d, tao):
 
     for n in kMax:
         # If a value doesn't exists, we assume that it's 0
-        if cn[n-1]==None:  cn[n-1].append(0)
+        if cn[n-1] == None:
+            cn[n-1].append(0)
         cn.append(a1*cn[n-1] + b1*cn[n-1-d])
-    
+
     return cn
 
+
 # Equations' Table
-headers = { 'k': [], 'y(k)': [], 'u(k)': [] }
+headers = {'k': [], 'y(k)': [], 'u(k)': []}
 
 table = pd.DataFrame(headers)
 headings = list(headers)
@@ -134,46 +156,62 @@ layout = [
     # [sg.Frame(layout=[
     #     [sg.Table(values=values, headings=headings, auto_size_columns=False, col_widths=list(map(lambda x:len(x)+1, headings)))]
     # ], title='Table of Values')],
-    #Coeffiecients
-    [sg.Frame(layout= [
-        [sg.Text('Coefficients')],
+    # Coeffiecients
+    [sg.Frame(layout=[
         [sg.Text('a')],
-        *[[sg.Text('a' + str(i)), sg.InputText(),] for i in range(numA)],
+        *[[sg.Text('a' + str(i)), sg.InputText(), ] for i in range(numA)],
         [sg.Button('Add a'), sg.Button('Delete a')],
         [sg.Text('b')],
-        *[[sg.Text('b' + str(i)), sg.InputText(),] for i in range(numB)],
+        *[[sg.Text('b' + str(i)), sg.InputText(), ] for i in range(numB)],
         [sg.Button('Add b'), sg.Button('Delete b')],
     ], title='Coefficients')],
     # Input Values
-    [sg.Text('Enter Values')],
-    [sg.Text('Constant (k)', size=defaultSize), sg.InputText(key='-k-', size=defaultSize), sg.Text('Delay (d)', size=defaultSize), sg.InputText(key='-d-', size=defaultSize)],
-    [sg.Text("\u03F4'", size=defaultSize), sg.InputText(key='-tPrime-', size=defaultSize), sg.Text('Time Interval (T)', size=defaultSize), sg.InputText(key='-T-', size=defaultSize)],
-    [sg.Text('Time Constant (\u03F4)', size=defaultSize), sg.InputText(key='-tau-', size=defaultSize), sg.Text('m[k]'), sg.InputText(key='-mk-', size=defaultSize)],
-    [sg.Text('Input Disturbance', size=defaultSize), sg.InputText(key='-inputD-', size=defaultSize), sg.Text('Output Disturbance', size=defaultSize), sg.InputText(key='-outputD-', size=defaultSize)],
-
+    [sg.Frame(layout=[
+        [sg.Text('Constant (k)', size=defaultSize), sg.InputText(key='-k-', size=defaultSize)],
+        [sg.Text('Delay (d)', size=defaultSize), sg.InputText(key='-d-', size=defaultSize)],
+        [sg.Text("\u03F4'", size=defaultSize), sg.InputText(key='-tPrime-', size=defaultSize)],
+        [sg.Text('Time Interval (T)', size=defaultSize), sg.InputText(key='-T-', size=defaultSize)]
+    ], title='Values'),
+    sg.Frame(layout=[
+        [sg.Text('Time Constant (\u03F4)', size=defaultSize), sg.InputText(key='-tau-', size=defaultSize)], 
+        [sg.Text('m[k]'), sg.InputText(key='-mk-', size=defaultSize)],
+        [sg.Text('Input Disturbance', size=defaultSize), sg.InputText(key='-inputD-', size=defaultSize)],
+        [sg.Text('Output Disturbance', size=defaultSize), sg.InputText(key='-outputD-', size=defaultSize)]
+    ], title='')],
     # Window Buttons
     [sg.Button('Submit'), sg.Button('Close')]
 ]
 
 # Create the window
 window = sg.Window(
-    'Discrete Control Model', # Window Title
+    'Discrete Control Model',  # Window Title
     location=(0, 0),
     finalize=True,
     element_justification='center',
     font='Helvetica 18',
     size=(800, 800)
-).Layout(layout).Finalize() # Window Layout
-window.maximize()
+).Layout(layout).Finalize()  # Window Layout
 
 # Graph
 fig = matplotlib.figure.Figure(figsize=(5, 4), dpi=100)
-t = np.arange(0, 3, .01)
-fig.add_subplot(111).plot(t, 2 * np.sin(1 + np.pi * t))
+
+# Define Transfer Function
+num = np.array([2])
+den = np.array([3, 1])
+
+H = signal.TransferFunction(num, den)
+t, y = signal.step(H)
+
+plt.plot(t, y)
+plt.title('Step Response')
+plt.xlabel('t')
+plt.ylabel('y')
+plt.grid()
+
+fig.add_subplot(111).plot(plt.show())
 
 # Add plot to the window
 draw_figure(window['-CANVAS-'].TKCanvas, fig)
-#draw_figure(window['-CANVAS-'].TKCanvas, fig2)
 
 # Create an event loop
 while True:
@@ -194,45 +232,64 @@ while True:
 
     # If a new coefficient a is added
     if event == 'Add a' or event == 'Delete a':
-        if event == 'Delete a': numA += -1  
-        elif numA <= maxCoefficientIndex: numA += 1
-        else: print('\nNo more than 4 coefficients can be declared.')
+        if event == 'Delete a':
+            numA += -1
+        elif numA <= maxCoefficientIndex:
+            numA += 1
+        else:
+            print('\nNo more than 4 coefficients can be declared.')
 
         # Avoid reusing layout
         layout = [
             # Canvas
             [sg.Text('Discrete Control Model Grapher')],
             [sg.Canvas(key='-CANVAS-')],
-            #Coeffiecients
+            # Coeffiecients
             [sg.Text('Coefficients')],
             [sg.Text('a')],
-            *[[sg.Text('a' + str(i)), sg.InputText(),] for i in range(numA)],
+            *[[sg.Text('a' + str(i)), sg.InputText(), ] for i in range(numA)],
             [sg.Button('Add a'), sg.Button('Delete a')],
             [sg.Text('b')],
-            *[[sg.Text('b' + str(i)), sg.InputText(),] for i in range(numB)],
+            *[[sg.Text('b' + str(i)), sg.InputText(), ] for i in range(numB)],
             [sg.Button('Add b'), sg.Button('Delete b')],
             # Input Values
             [sg.Text('Enter Values')],
-            [sg.Text('Constant (k)', size=defaultSize), sg.InputText(key='-k-', size=defaultSize), sg.Text('Delay (d)', size=defaultSize), sg.InputText(key='-d-', size=defaultSize)],
-            [sg.Text("\u03F4'", size=defaultSize), sg.InputText(key='-tPrime-', size=defaultSize), sg.Text('Time Interval (T)', size=defaultSize), sg.InputText(key='-T-', size=defaultSize)],
-            [sg.Text('Time Constant (\u03F4)', size=defaultSize), sg.InputText(key='-t-', size=defaultSize), sg.Text('m[k]'), sg.InputText(key='-mk-', size=defaultSize)],
-            [sg.Text('Input Disturbance', size=defaultSize), sg.InputText(key='-inputD-', size=defaultSize), sg.Text('Output Disturbance', size=defaultSize), sg.InputText(key='-outputD-', size=defaultSize)],
+            [sg.Text('Constant (k)', size=defaultSize), sg.InputText(key='-k-', size=defaultSize),
+             sg.Text('Delay (d)', size=defaultSize), sg.InputText(key='-d-', size=defaultSize)],
+            [sg.Text("\u03F4'", size=defaultSize), sg.InputText(key='-tPrime-', size=defaultSize),
+             sg.Text('Time Interval (T)', size=defaultSize), sg.InputText(key='-T-', size=defaultSize)],
+            [sg.Text('Time Constant (\u03F4)', size=defaultSize), sg.InputText(
+                key='-t-', size=defaultSize), sg.Text('m[k]'), sg.InputText(key='-mk-', size=defaultSize)],
+            [sg.Text('Input Disturbance', size=defaultSize), sg.InputText(key='-inputD-', size=defaultSize),
+             sg.Text('Output Disturbance', size=defaultSize), sg.InputText(key='-outputD-', size=defaultSize)],
             # Window Buttons
             [sg.Button('Submit'), sg.Button('Close')]
         ]
 
         windowTemp = sg.Window('Discrete Model Grapher', location=(0, 0),
-        finalize=True, element_justification='center', font='Helvetica 18').Layout(layout).Finalize()
+                               finalize=True, element_justification='center', font='Helvetica 18').Layout(layout).Finalize()
         window.Close()
         window = windowTemp
-        window.maximize()
 
         fig = matplotlib.figure.Figure(figsize=(5, 4), dpi=100)
-        t = np.arange(0, 3, .01)
-        fig.add_subplot(111).plot(t, 2 * np.sin(1 + np.pi * t))
-        
+
+        # Define Transfer Function
+        num = np.array([2])
+        den = np.array([3, 1])
+
+        H = signal.TransferFunction(num, den)
+        t, y = signal.step(H)
+
+        plt.plot(t, y)
+        plt.title('Step Response')
+        plt.xlabel('t')
+        plt.ylabel('y')
+        plt.grid()
+
+        fig.add_subplot(111).plot(plt.show())
+
         draw_figure(window['-CANVAS-'].TKCanvas, fig)
-                
+
         # Store submitted values in global variables
         k = values['-k-']
         T = values['-T-']
@@ -242,45 +299,63 @@ while True:
         tau = values['-tau-']
         mk = values['-mk-']
 
-
     # If another coefficient b is added
     if event == 'Add b' or event == 'Delete b':
-        if event == 'Delete b': numB += -1  
-        elif numB <= maxCoefficientIndex: numB += 1
-        else: print('\nUnable to add more than 4 coeefficients.')
+        if event == 'Delete b':
+            numB += -1
+        elif numB <= maxCoefficientIndex:
+            numB += 1
+        else:
+            print('\nUnable to add more than 4 coeefficients.')
 
         # Avoid reusing layout
         layout = [
             # Canvas
             [sg.Text('Discrete Control Model Grapher')],
             [sg.Canvas(key='-CANVAS-')],
-            #Coeffiecients
+            # Coeffiecients
             [sg.Text('Coefficients')],
             [sg.Text('a')],
-            *[[sg.Text('a' + str(i)), sg.InputText(),] for i in range(numA)],
+            *[[sg.Text('a' + str(i)), sg.InputText(), ] for i in range(numA)],
             [sg.Button('Add a'), sg.Button('Delete a')],
             [sg.Text('b')],
-            *[[sg.Text('b' + str(i)), sg.InputText(),] for i in range(numB)],
+            *[[sg.Text('b' + str(i)), sg.InputText(), ] for i in range(numB)],
             [sg.Button('Add b'), sg.Button('Delete b')],
             # Input Values
             [sg.Text('Enter Values')],
-            [sg.Text('Constant (k)', size=defaultSize), sg.InputText(key='-k-', size=defaultSize), sg.Text('Delay (d)', size=defaultSize), sg.InputText(key='-d-', size=defaultSize)],
-            [sg.Text("\u03F4'", size=defaultSize), sg.InputText(key='-tPrime-', size=defaultSize), sg.Text('Time Interval (T)', size=defaultSize), sg.InputText(key='-T-', size=defaultSize)],
-            [sg.Text('Time Constant (\u03F4)', size=defaultSize), sg.InputText(key='-t-', size=defaultSize), sg.Text('m[k]'), sg.InputText(key='-mk-', size=defaultSize)],
-            [sg.Text('Input Disturbance', size=defaultSize), sg.InputText(key='-inputD-', size=defaultSize), sg.Text('Output Disturbance', size=defaultSize), sg.InputText(key='-outputD-', size=defaultSize)],
+            [sg.Text('Constant (k)', size=defaultSize), sg.InputText(key='-k-', size=defaultSize),
+             sg.Text('Delay (d)', size=defaultSize), sg.InputText(key='-d-', size=defaultSize)],
+            [sg.Text("\u03F4'", size=defaultSize), sg.InputText(key='-tPrime-', size=defaultSize),
+             sg.Text('Time Interval (T)', size=defaultSize), sg.InputText(key='-T-', size=defaultSize)],
+            [sg.Text('Time Constant (\u03F4)', size=defaultSize), sg.InputText(
+                key='-t-', size=defaultSize), sg.Text('m[k]'), sg.InputText(key='-mk-', size=defaultSize)],
+            [sg.Text('Input Disturbance', size=defaultSize), sg.InputText(key='-inputD-', size=defaultSize),
+             sg.Text('Output Disturbance', size=defaultSize), sg.InputText(key='-outputD-', size=defaultSize)],
             # Window Buttons
             [sg.Button('Submit'), sg.Button('Close')]
         ]
 
         windowTemp = sg.Window('Discrete Model Grapher', location=(0, 0),
-        finalize=True, element_justification='center', font='Helvetica 18').Layout(layout).Finalize()
+                               finalize=True, element_justification='center', font='Helvetica 18').Layout(layout).Finalize()
         window.Close()
         window = windowTemp
-        window.maximize()
 
         fig = matplotlib.figure.Figure(figsize=(5, 4), dpi=100)
-        t = np.arange(0, 3, .01)
-        fig.add_subplot(111).plot(t, 2 * np.sin(1 + np.pi * t))
+
+        # Define Transfer Function
+        num = np.array([2])
+        den = np.array([3, 1])
+
+        H = signal.TransferFunction(num, den)
+        t, y = signal.step(H)
+
+        plt.plot(t, y)
+        plt.title('Step Response')
+        plt.xlabel('t')
+        plt.ylabel('y')
+        plt.grid()
+
+        fig.add_subplot(111).plot(plt.show())
 
         draw_figure(window['-CANVAS-'].TKCanvas, fig)
 
